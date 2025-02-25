ad1b6b027d2bc03fd6ff12ccd6be000cac785b90
Harco Kuppens <h.kuppens@gmail.com>
2025-02-25 13:07:59 +0100

    improved launch script

--- example/launch_in_webserver.bash
+++ example/launch_in_webserver.bash
@@ -1,4 +1,6 @@
+pkill 'serve -l 5555'
+sleep 1
 npx serve -l 5555 &                   # launch webserver on port 5555
 sleep 2                               # wait until server is up
 open http://localhost:5555/index.html # open html file in webserver
-fg                                    # put server back on foreground. Kill with CTRL-C, so that we can relaunch later.
+wait                                  # put server back on foreground. Kill with CTRL-C, so that we can relaunch later.
